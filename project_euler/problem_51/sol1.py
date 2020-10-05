399ddda7e31471f0047f2d1c5252d8de4fe140a2
Roland Metivier <metivier.roland@chlorophyt.us>
2020-10-05 12:31:57 -0500

    Add (untestable currently) solution for 51

--- project_euler/problem_51/sol1.py
+++ project_euler/problem_51/sol1.py
@@ -13,41 +13,59 @@ smallest prime with this property.
 Find the smallest prime which, by replacing part of the number (not necessarily
 adjacent digits) with the same digit, is part of an eight prime value family.
 """
-import math
+from math import log10
+from itertools import product
+from typing import Tuple, Optional
+
+MAX_BRUTE_FORCE_LOG: int = 6
 
 
-"""
-https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-"""
 def sieve_of_eratosthenes(maximum: int) -> [int]:
+    """
+    https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    """
     sieve: [int] = [*range(2, maximum)]
     primes: [int] = []
     while len(sieve) > 0:
         primes.append(sieve[0])
-        sieve = [s for s in sieve[1:] if not s % sieve[0] == 0]
+        sieve = [s for s in sieve[1:] if s % sieve[0] != 0]
     return primes
-        
-    # composites: [[int]] = []
-    # for n in range(2, math.isqrt(maximum)):
-    #     composites.append([m for m in range(n, maximum) if m % n == 0])
-    # sieve: [int] = [c for deep in composites for c in deep]
-    # return sorted([prime for prime in sieve if sieve.count(prime) == 1])
 
-def get_digit_combinations(comb: int) -> [[int]]:
-    log10: int = round(math.log10(comb) - 0.5) + 1
-    combinations: [[int]] = []
-    for digit in range(0, log10):
-        this_digit: int = 10 ** digit
-        combinations.append([d for d in range(
-            this_digit, this_digit * 10, this_digit)])
-    return combinations
+
+def digit_combinations(logarithm: int) -> [Tuple[Optional[int]]]:
+    """
+    Return combinations of wildcard None and digits for parameter `logarithm`.
+
+    :param logarithm The length of the tuples returned.
+    """
+    def splice(candidates: Tuple[Optional[int]]) -> Tuple[Optional[int]]:
+        return tuple((10 ** (logarithm - n - 1) * d if d is not None else None for n, d in enumerate(candidates)))
+    return (i for i in map(splice, product([None, *range(0, 10)], repeat=logarithm)) if i.count(None) > 1)
+
+
+PRIMES_CACHE: [int] = sieve_of_eratosthenes(2 ** MAX_BRUTE_FORCE_LOG)
+DIGITS_CACHE: [Tuple[Optional[int]]] = [*digit_combinations(MAX_BRUTE_FORCE_LOG)]
 
 
 def solution():
-    pass
-    # sieve: [int] = sieve_of_eratosthenes(10000)
-    # for i in range(10, 56003):
-    #     primes = sieve_of_eratosthenes(i)
+    for digit_combination in DIGITS_CACHE:
+        logarithm_candidates: [int] = [
+            n for n, i in enumerate(digit_combination) if i is None]
+        logarithm_fusion_bases: [int] = range(10)
+        number_fusion: int = sum(
+            [c for c in digit_combination if c is not None])
+        for n, i in enumerate(logarithm_fusion_bases):
+            i = sum([10 ** candidate * n for candidate in logarithm_candidates])
+        accumulator: int = 0
+        number: Optional[int] = None
+        for i in logarithm_fusion_bases:
+            if number_fusion + i in PRIMES_CACHE:
+                accumulator += 1
+                if number is None:
+                    number = number_fusion + i
+        if accumulator > 5:
+            return number
+    return None
 
 
 if __name__ == "__main__":
